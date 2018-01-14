fdb7bddc0aff8a53ba76c9e92240153a1f05fe1b
Squnix <kunakunicki97@gmail.com>
2018-01-14 13:07:47 +0100

    Dodanie godzin otwarcia do formularza

--- kontakt.php
+++ kontakt.php
@@ -7,5 +7,5 @@
 <p>Telefon: 566-545-343</p>
 <p>Adres: Magnuszewksa 23/2</p>
 <p>Miasto: Bydgoszcz 11-234</p>
-<p>Eamil: Email@domena.pl</p>
-
+<p>Email: Email@domena.pl</p>
+<p>Otwarte w godzinach : 07:00-18:00</p>
